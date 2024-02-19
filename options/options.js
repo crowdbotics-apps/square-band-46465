4850332381f8a265f8e8963ea54f138b8d38d094
Crowdbotics <team@crowdbotics.com>
2024-02-19 16:08:17 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "square-band-46465",
+  url: "https://square-band-46465.botics.co",
+  api: "https://square-band-46465.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
